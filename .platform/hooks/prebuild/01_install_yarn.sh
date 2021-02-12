2c806a7739e3e8a86951b037737891e0fe243187
quatro <ch.walker@gmail.com>
2021-02-12 12:27:30 -0500

    Updated config

--- .platform/hooks/prebuild/01_install_yarn.sh
+++ .platform/hooks/prebuild/01_install_yarn.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 set -xe
+
 # Install nodejs
 echo "install nodejs"
 curl --silent --location https://rpm.nodesource.com/setup_15.x | sudo bash -
 yum -y install nodejs
-echo "install yarn"
+
 # install yarn
+echo "install yarn"
 wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo;
 yum -y install yarn;
+
+# install webpack-cli with yarn
+echo "install webpack-cli"
+cd "${app}";
+yarn add webpack-cli;
