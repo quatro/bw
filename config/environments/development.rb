0d1c25d49571d2091ef02381cb18df1a27bfee80
quatro <ch.walker@gmail.com>
2021-03-08 13:54:13 -0500

    Fixes to sending email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,12 +63,12 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-      :user_name => ENV.fetch('SENDGRID_API_KEY'),
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+      :authentication => :plain,
+      :user_name => 'apikey',
       :password => ENV.fetch('SENDGRID_API_KEY'),
       :domain => 'heatbooking.com',
-      :address => 'smtp.sendgrid.net',
-      :port => 587,
-      :authentication => :plain,
       :enable_starttls_auto => true
   }
 
