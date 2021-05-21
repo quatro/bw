dd16f9aa136e260cdf186a872ce9c3cd709ea7bb
quatro <ch.walker@gmail.com>
2021-05-21 13:52:42 -0400

    Updated migration

--- db/migrate/20210521173259_add_internal_tax.rb
+++ db/migrate/20210521173259_add_internal_tax.rb
@@ -1,5 +1,10 @@
 class AddInternalTax < ActiveRecord::Migration[6.1]
   def change
     add_column :booking_rooms, :internal_tax, :decimal, precision: 8, scale: 2
+
+
+    BookingRoom.calculate_totals
+
+    BookingRoom.all.each{|a| a.denormalize}
   end
 end
